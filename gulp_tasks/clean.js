0e8b5eecd2889c1cae02ed2be70dfb14133ba108
JordyPouw <jordypouw@gmail.com>
2015-11-28 00:52:20 +0100

    gulp clean - make dirs dynamic from config.js

--- gulp_tasks/clean.js
+++ gulp_tasks/clean.js
@@ -4,10 +4,18 @@ module.exports = function(gulp){
 
 
   var config = require('./config')();
+  var dirs = [
+                config.source.dest.css,
+                config.source.dest.js,
+                config.source.dest.img,
+                config.source.dest.font,
+                config.source.dest.sound,
+                config.source.tmp
+             ];
 
   gulp.task('clean', function(callback){
 
-    config.del(['dest/', 'tmp/'], callback);
+    config.del(dirs, callback);
 
   });
 
