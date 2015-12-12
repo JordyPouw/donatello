b110cd9c243fbb9e7ff9448e2013aa25927b41ff
JordyPouw <jordypouw@gmail.com>
2015-12-12 02:25:33 +0100

    create server task

---
+++ gulp_tasks/server.js
@@ -0,0 +1,19 @@
+module.exports = function (gulp) {
+
+  'use strict';
+
+  
+  var config = require('./config')();
+  var path = require('path');
+  
+  gulp.task('server', function () {
+
+    config.plugins.connect.server({
+      root: path.resolve('./'),
+      port: 8000
+    });
+
+  });
+
+
+};
