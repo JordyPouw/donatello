cc8bf12b97c21fb38e57a04c15373a5ac8a87959
JordyPouw <jordypouw@gmail.com>
2016-07-14 17:23:20 +0200

    clean code smooth-scrolling

--- src/js/smooth-scrolling.js
+++ src/js/smooth-scrolling.js
@@ -4,18 +4,15 @@
 
 
   smoothScrolling();
-  
+
   function smoothScrolling () {
-    $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').on('click', function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') &&
+          location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
+
+        if (target.length) { $('body').animate({ scrollTop: target.offset().top }, 1000); }
       }
     });
   }
