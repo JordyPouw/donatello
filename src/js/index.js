4ddefd7aa59c4621e027baef319693ef042655ff
JordyPouw <jordypouw@gmail.com>
2016-07-26 12:49:47 +0200

    update index js

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,7 @@
   require('./fluid-embed-vids.js');
   require('./smooth-scrolling.js');
   require('./components.image-broken.js');
+  require('./components.form.js');
   require('./utilities.disable-hover.js');
 
 
