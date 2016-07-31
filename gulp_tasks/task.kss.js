e49eb77dd87381d791cb3e578fa0f9f23e065ba1
JordyPouw <jordypouw@gmail.com>
2016-07-31 23:51:10 +0200

    place all kss sources in kss dir

--- gulp_tasks/task.kss.js
+++ gulp_tasks/task.kss.js
@@ -5,10 +5,19 @@ module.exports = function (gulp) {
 
   var config = require('./config')();
 
+  // if OS is Mac.
+  if (process.platform === 'darwin') { var open = 'open'; }
+
+  // if OS is Linux.
+  else if (process.platform === 'linux') { var open = 'xdg-open'; }
+
+  // if OS is Windows.
+  else if (process.platform === 'win32') { var open = 'start'; }
+
   gulp.task('kss', config.plugins.shell.task([
 
     'kss --config kss-config.json',
-    'open kss_styleguide/index.html'
+    open + ' kss_styleguide/styleguide/index.html'
 
   ]));
 
